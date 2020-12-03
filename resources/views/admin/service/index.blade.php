d6b3203f09c1898839420e5097d245ea45200aa1
Purple <Joseph@eftechnology.net>
2020-12-03 18:14:20 +0100

    Start work on service permission

---
+++ resources/views/admin/service/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.admin')
+
+@section('title')
+Service | 
+@endsection
+
+@section('content')
+
+
+
+
+@endsection
--- resources/views/layouts/backend_partials/status.blade.php
+++ resources/views/layouts/backend_partials/status.blade.php
@@ -11,12 +11,13 @@
 
 
 
-@if (session('success'))
+@if (session('status'))
 <div class="alert alert-success alert-dismissible">
 	<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
 	<h4><i class="icon fa fa-check"></i> Alert!</h4>
-	{{ session('success') }}
+	{{ session('status') }}
 </div>
 @endif
+
 
 
