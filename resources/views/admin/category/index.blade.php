9ca20ae0dba3e474dda0afeb7df966c5307b66f8
Purple <Joseph@eftechnology.net>
2020-12-04 10:57:48 +0100

    Change category route

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -1,12 +1,46 @@
+
 @extends('layouts.admin')
 
 @section('title')
-Category List | 
+Category | 
 @endsection
 
 @section('content')
 
-@include('admin/section/category_table')
+<div class="container">
+
+  @include('layouts.backend_partials.status')
+
+<div class="box box-info mt-5">
+  <div class="box-header with-border">
+    <h3 class="box-title"> Category Form </h3>
+  </div>
+  <!-- /.box-header -->
+  <!-- form start -->
+  <form class="form-horizontal form-element" method="POST" action="{{route('admin.category.store')}}" >
+    
+    {{ csrf_field() }}
+
+    <div class="box-body">
+      <div class="form-group">
+        <label for="inputEmail3" class="col-sm-2 control-label"> Name </label>
+
+        <div class="col-sm-10">
+          <input type="text" class="form-control" name="name" id="inputEmail3" placeholder="Enter Category Name Here" required>
+        </div>
+      </div>
+    <!-- /.box-body -->
+    <div class="box-footer">
+      <button type="submit" class="btn btn-info pull-right"> Submit </button>
+    </div>
+    <!-- /.box-footer -->
+  </form>
+</div>
+
+</div>
+
+
+@include('admin/section/category_table') 
+
 
 @endsection
-
