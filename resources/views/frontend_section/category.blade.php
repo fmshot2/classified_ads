abb4a8427f6a407007c2e032154c0b198c97a751
Ibidapo Adeolu <adeoluibidapo@gmail.com>
2021-01-26 14:41:35 +0100

    Update category.blade.php

--- resources/views/frontend_section/category.blade.php
+++ resources/views/frontend_section/category.blade.php
@@ -14,22 +14,21 @@
 
                     <div class="popular-posts featured-service-hm">
                         @foreach($featuredServices as $featuredService)
-                        <div class="media p-2">
-                            <a href="{{route('serviceDetail', $featuredService->slug)}}">
-                                <div class="media-left">
-                                    <img class="media-object" src=" {{asset('images')}}/{{ $featuredService->image[0]}} " alt="sub-properties">
-                                </div>
-                            </a>
-                            <div class="media-body align-self-center">
-                                <h3 class="media-heading"><a href="{{route('serviceDetail', $featuredService->slug)}}">{{ Str::limit($featuredService->name, 30)}}</a></h3>
+                            <div class="media p-2">
+                                <a href="{{route('serviceDetail', $featuredService->slug)}}">
+                                    <div class="media-left">
+                                        <img class="media-object" src=" {{asset('images')}}/{{ $featuredService->image[0]}} " alt="sub-properties">
+                                    </div>
+                                </a>
+                                <div class="media-body align-self-center">
+                                    <h3 class="media-heading"><a href="{{route('serviceDetail', $featuredService->slug)}}">{{ Str::limit($featuredService->name, 30)}}</a></h3>
 
-                                <p class="fea-ad-hm-location"><strong>Location:</strong> <a href="{{route('serviceDetail', $featuredService->slug)}}">{{ Str::limit($featuredService->state, 30)}}</a></p>
+                                    <p class="fea-ad-hm-location"><strong>Location:</strong> <a href="{{route('serviceDetail', $featuredService->slug)}}">{{ Str::limit($featuredService->state, 30)}}</a></p>
+                                </div>
                             </div>
-                        </div>
                         @endforeach
                     </div>
                 </div>
-
             </div>
 
 
@@ -37,7 +36,6 @@
                 <div class="main-title">
                     <h1>What service are you looking for?</h1>
                 </div>
-                {{-- <div class="sidebar-right"> --}}
                 <div class="">
                     <div class="row" style="visibility: visible;">
                         @if(isset($categories))
@@ -79,7 +77,7 @@
                                             </div>
                                         </a>
 
-                                        <a href="{{route('services', $category->slug)}}" >
+                                        <a href="{{route('services', $category->slug)}}">
                                             <h6  class="cat-title">{{$category->name}}</h6>
                                         </a>
                                     </div>
@@ -131,75 +129,53 @@
                         <div class="m-border"></div>
                     </div>
 
-                    {{-- <div class="">
-                          <img src="{{asset('images')}}/{{'MTN-apptitude.jpg'}}" alt="advert" class="img-fluid featured-ad-hm-image">
-                    </div><br>
-                    <div class="s-border"></div>
-                    <div class="m-border"></div> --}}
-
                     <div class="popular-posts featured-ad-hm-list">
-                   {{--      @foreach($featuredServices as $featuredService)
-                        <div class="media p-2">
-                            <div class="media-left">
-                                <img class="media-object" src=" {{asset('images')}}/{{ $featuredService->image[0]}} " alt="sub-properties">
-                            </div>
-                            <div class="media-body align-self-center">
-                                <h3 class="media-heading"><a href="#">{{ Str::limit($featuredService->name, 35)}}</a></h3>
-                                <p class="fea-ad-hm-location"><strong>Location:</strong> {{ Str::limit($featuredService->state, 30)}}</p>
-                            </div>
-                        </div>
-                        @endforeach --}}
-
-
-
-
-<div class="container">
-    <div id="carouselExampleControls" class="carousel vert slide" data-ride="carousel" data-interval="900">
-        <ol class="carousel-indicators">
-            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-        </ol>
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img class="d-block mx-auto img-fluid" src="{{asset('images')}}/{{'MTN-apptitude.jpg'}}" alt="First slide">
-            </div>
-            <div class="carousel-item">
-                <img class="d-block mx-auto img-fluid" src="{{asset('images')}}/{{'pepsi.jpg'}}" alt="Second slide">
-            </div>
-            <div class="carousel-item">
-                <img class="d-block mx-auto img-fluid" src="{{asset('images')}}/{{'MTN-apptitude.jpg'}}" alt="Third slide">
-            </div>
-        </div>
-        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon bg-dark rounded-circle" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-            <span class="carousel-control-next-icon bg-dark rounded-circle" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>
-    </div>
-</div>
-
- @foreach($featuredServices as $featuredService)
-                        <div class="media p-2">
-                            <div class="media-left">
-                                <img class="media-object" src=" {{asset('images')}}/{{ $featuredService->image[0]}} " alt="sub-properties">
-                            </div>
-                            <div class="media-body align-self-center">
-                                <h3 class="media-heading"><a href="#">{{ Str::limit($featuredService->name, 35)}}</a></h3>
-                                <p class="fea-ad-hm-location"><strong>Location:</strong> {{ Str::limit($featuredService->state, 30)}}</p>
+                        <div class="container">
+                            <div id="carouselExampleControls" class="carousel vert slide" data-ride="carousel" data-interval="900">
+                                <ol class="carousel-indicators">
+                                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                                </ol>
+                                <div class="carousel-inner">
+                                    <div class="carousel-item active">
+                                        <img class="d-block mx-auto img-fluid" src="{{asset('images')}}/{{'MTN-apptitude.jpg'}}" alt="First slide">
+                                    </div>
+                                    <div class="carousel-item">
+                                        <img class="d-block mx-auto img-fluid" src="{{asset('images')}}/{{'pepsi.jpg'}}" alt="Second slide">
+                                    </div>
+                                    <div class="carousel-item">
+                                        <img class="d-block mx-auto img-fluid" src="{{asset('images')}}/{{'MTN-apptitude.jpg'}}" alt="Third slide">
+                                    </div>
+                                </div>
+                                <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                                    <span class="carousel-control-prev-icon bg-dark rounded-circle" aria-hidden="true"></span>
+                                    <span class="sr-only">Previous</span>
+                                </a>
+                                <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                                    <span class="carousel-control-next-icon bg-dark rounded-circle" aria-hidden="true"></span>
+                                    <span class="sr-only">Next</span>
+                                </a>
                             </div>
                         </div>
+
+                        @foreach($featuredServices as $featuredService)
+                            <div class="media p-2">
+                                <div class="media-left">
+                                    <img class="media-object" src=" {{asset('images')}}/{{ $featuredService->image[0]}} " alt="sub-properties">
+                                </div>
+                                <div class="media-body align-self-center">
+                                    <h3 class="media-heading"><a href="#">{{ Str::limit($featuredService->name, 35)}}</a></h3>
+                                    <p class="fea-ad-hm-location"><strong>Location:</strong> {{ Str::limit($featuredService->state, 30)}}</p>
+                                </div>
+                            </div>
                         @endforeach
-
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
-
 
 
 
