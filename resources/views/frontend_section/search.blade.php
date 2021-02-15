db458bf33a7fd97ea8ef5bc311e08013344f4136
fmshot2 <femishotolaa@gmail.com>
2021-02-15 19:38:07 +0100

    removed the second rang input that has form control class in search blade

--- resources/views/frontend_section/search.blade.php
+++ resources/views/frontend_section/search.blade.php
@@ -38,7 +38,7 @@
                     <div class="col-lg-2 col-md-4 col-sm-6 text-center">
                         <p style="font-weight: 600; margin-bottom: 0;">Choose Distance(in km): <span id="demo"></span></p>
                         <div class="slidecontainer" style="margin-bottom: 15px;">
-                            <input type="range" min="1" max="100" value="50" class="slider form-control" id="myRange2">
+                            {{-- <input type="range" min="1" max="100" value="50" class="slider form-control" id="myRange2"> --}}
                             <input type="range" min="1" max="100" name="ranges"  value="50" class="slider" id="myRange">
                         </div>
                     </div>
