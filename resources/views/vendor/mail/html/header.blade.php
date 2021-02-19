103d1923427b8f1da6b4a60eaab51688dacad90c
Ibidapo Adeolu <adeoluibidapo@gmail.com>
2021-02-19 12:38:08 +0100

    all

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,6 +1,6 @@
 <tr>
 <td class="header">
-<a href="{{ $url }}" style="display: inline-block;">
+<a href="{{ $url }}" style="display: inline-block;color: #fff">
 @if (trim($slot) === 'EFContact')
 <img src="{{ asset('logos/efcontactlogo.png') }}" class="logo" alt="EFContact Logo">
 @else
