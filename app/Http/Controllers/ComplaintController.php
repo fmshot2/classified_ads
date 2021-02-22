b67259323686200e642314e2465d24444e3b2f35
Ibidapo Adeolu <adeoluibidapo@gmail.com>
2021-02-22 13:46:28 +0100

    Update ComplaintController.php

--- app/Http/Controllers/ComplaintController.php
+++ app/Http/Controllers/ComplaintController.php
@@ -22,19 +22,19 @@ class ComplaintController extends Controller
 			'buyer_email' => 'required',
 			'description' => 'required',
 
-		]); 
+		]);
         /*$message->buyer_id = $request->buyer_id;
         $message->service_id = $request->service_id;
         $message->description = $request->description;*/
         $success = 'Your message was sent successfully. Thank you!';
         $slug = Str::random(10);
-        $complaint = new Complaint();  
+        $complaint = new Complaint();
 
-                //$message->service_id = $data['id']; 
+                //$message->service_id = $data['id'];
         $complaint->buyer_id = $data['buyer_id'];
-        $complaint->buyer_name = $data['buyer_name'];     
+        $complaint->buyer_name = $data['buyer_name'];
         $complaint->buyer_email = $data['buyer_email'];
-        $complaint->slug = $slug; 
+        $complaint->slug = $slug;
         $complaint->service_id = $data['service_id'];
         $complaint->service_user_id = $data['service_user_id'];
         $complaint->description = $data['description'];
@@ -48,7 +48,13 @@ class ComplaintController extends Controller
         if ($complaint->save()) {
         	$buyer_email = $complaint->buyer_email;
         	$name = 'Your message has been delivered successfully!';
-        	Mail::to($buyer_email)->send(new SendMailable($name));
+            try{
+                Mail::to($buyer_email)->send(new SendMailable($name));
+            }
+            catch(\Exception $e){
+                $failedtosendmail = 'Failed to Mail!.';
+            }
+
         	return response()->json(['success'=>'Ajax request submitted successfully', 'success2'=>$success]);
         }
         return response()->json(['success'=>'Ajax request submitted successfully', 'success2'=>"not saved"]);
