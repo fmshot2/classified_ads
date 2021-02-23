773399b84d1f885d7ddaaba5d6e5b094e817c06c
Ibidapo Adeolu <adeoluibidapo@gmail.com>
2021-02-23 14:01:23 +0100

    search result page

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -674,7 +674,7 @@ public function search3(Request $request){
   //   $query->where('name', 'like', '%' . $keyword . '%');
   // })->get();
 
- $services1 = Service::selectRaw("id, name, address, thumbnail, user_id, state, badge_type, category_id,
+ $services1 = Service::selectRaw("id, name, slug, address, thumbnail, user_id, state, badge_type, category_id,
    ( 6371000 * acos( cos( radians(?) ) *
    cos( radians( latitude ) )
                        * cos( radians( longitude ) - radians(?)
@@ -693,7 +693,7 @@ public function search3(Request $request){
 
 if ($category)
 {
-$services2 = Service::selectRaw("id, name, address, thumbnail, user_id, state, badge_type, category_id,
+$services2 = Service::selectRaw("id, name, slug, address, thumbnail, user_id, state, badge_type, category_id,
    ( 6371000 * acos( cos( radians(?) ) *
    cos( radians( latitude ) )
                        * cos( radians( longitude ) - radians(?)
@@ -713,7 +713,7 @@ $services2 = Service::selectRaw("id, name, address, thumbnail, user_id, state, b
 
   if ($state)
 {
-   $services3 = Service::selectRaw("id, name, address, thumbnail, user_id, state, badge_type, category_id,
+   $services3 = Service::selectRaw("id, name, slug, address, thumbnail, user_id, state, badge_type, category_id,
    ( 6371000 * acos( cos( radians(?) ) *
    cos( radians( latitude ) )
                        * cos( radians( longitude ) - radians(?)
@@ -737,7 +737,7 @@ $services2 = Service::selectRaw("id, name, address, thumbnail, user_id, state, b
 
 if($category && $state)
 {
-  $services4 = Service::selectRaw("id, name, address, thumbnail, user_id, state, badge_type, category_id,
+  $services4 = Service::selectRaw("id, name, slug, address, thumbnail, user_id, state, badge_type, category_id,
    ( 6371000 * acos( cos( radians(?) ) *
    cos( radians( latitude ) )
                        * cos( radians( longitude ) - radians(?)
@@ -759,7 +759,7 @@ if($category && $state)
 
 if($keyword && $state)
 {
-  $services5 = Service::selectRaw("id, name, address, thumbnail, user_id, state, badge_type, category_id,
+  $services5 = Service::selectRaw("id, name, slug, address, thumbnail, user_id, state, badge_type, category_id,
    ( 6371000 * acos( cos( radians(?) ) *
    cos( radians( latitude ) )
                        * cos( radians( longitude ) - radians(?)
@@ -783,7 +783,7 @@ if($keyword && $state)
 
 if($keyword && $state)
 {
-  $services6 = Service::selectRaw("id, name, address, thumbnail, user_id, state, badge_type, category_id,
+  $services6 = Service::selectRaw("id, name, slug, address, thumbnail, user_id, state, badge_type, category_id,
    ( 6371000 * acos( cos( radians(?) ) *
    cos( radians( latitude ) )
                        * cos( radians( longitude ) - radians(?)
@@ -806,7 +806,7 @@ if($keyword && $state)
 
 if($state)
 {
-  $services7 = Service::selectRaw("id, name, address, thumbnail, user_id, state, badge_type, category_id,
+  $services7 = Service::selectRaw("id, name, slug, address, thumbnail, user_id, state, badge_type, category_id,
    ( 6371000 * acos( cos( radians(?) ) *
    cos( radians( latitude ) )
                        * cos( radians( longitude ) - radians(?)
@@ -828,7 +828,7 @@ if($state)
 
 if($category)
 {
-  $services8 = Service::selectRaw("id, name, address, thumbnail, user_id, state, badge_type, category_id,
+  $services8 = Service::selectRaw("id, name, slug, address, thumbnail, user_id, state, badge_type, category_id,
    ( 6371000 * acos( cos( radians(?) ) *
    cos( radians( latitude ) )
                        * cos( radians( longitude ) - radians(?)
