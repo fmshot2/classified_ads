de923c507e267ecd491fed12e749b0c647c11c8d
Ibidapo Adeolu <adeoluibidapo@gmail.com>
2021-03-02 12:44:49 +0100

    feedback

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,92 @@
 	@include('layouts.frontend_partials.footer')
 
 
+    {{-- FEEDBACK MODULE  --}}
+    <a href="#" data-toggle="modal" data-target="#launchFeedback" class="float-feedback  animate__animated animate__fadeInRight">
+        <i class="fa fa-envelope"></i> Feedback
+    </a>
+
+    <style>
+        .float-feedback{
+            position:fixed;
+            width:120px;
+            height:50px;
+            top:90px;
+            right:0;
+            background-color:#03A84E;
+            color:#FFF;
+            font-size: 14px;
+            text-align:center;
+            font-weight: 400 !important;
+            box-shadow: 2px 2px 3px rgba(7, 7, 7, 0.493);
+            padding: 15px 10px;
+            border-bottom-left-radius: 50px;
+            border-top-left-radius: 50px;
+            z-index: 99999;
+            transition: background-color 1s;
+        }
+        .float-feedback:hover{
+            background-color:#0e8543;
+            color: rgb(230, 227, 227);
+        }
+        .my-float{
+            margin-top:22px;
+        }
+
+        @media (max-width: 768px){
+            .float-feedback{
+                width:90px;
+                height:30px;
+                font-size: 11px;
+                padding: 7px 10px;
+                top:60px;
+            }
+        }
+    </style>
+
+    <!-- Modal -->
+    <div id="launchFeedback" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+            <!-- Modal content-->
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Do you have a feedback for us?</h5>
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                </div>
+                <form id="feedbackForm" action="{{ route('feedback.form') }}" method="POST">
+                    @csrf
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <textarea name="userfeedback" id="userfeedback" class="form-control" cols="30" rows="5" placeholder="Tell us your experience on this site..." style="border-radius: 0"></textarea>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" onclick="submitFeedback()" class="btn btn-info" data-dismiss="modal">Send</button>
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    </div>
+                </form>
+            </div>
+
+        </div>
+    </div>
+
+    <script>
+        function submitFeedback() {
+            $.ajax({
+                url: '/user-feedback',
+                type: 'post',
+                dataType: 'json',
+                data: $('form#feedbackForm').serialize(),
+                success: function(data) {
+                    $('#userfeedback').val('')
+                    alert('Feedback sent! Thank You.')
+                }
+            });
+        }
+    </script>
+    {{--//... FEEDBACK MODULE  --}}
+
+
 	<a id="page_scroller" href="#scroll-top" style="position: fixed; z-index: 2147483647;"><i class="fa fa-chevron-up"></i></a>
 
 </body>
