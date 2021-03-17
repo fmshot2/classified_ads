1f4c277ceaf952500c7c0f6e16af2dd24bb7bee3
fmshot2 <femishotolaa@gmail.com>
2021-03-17 11:58:36 +0100

    added referer amount to agent table

--- database/migrations/2021_03_17_105733_add_referer_amount_to_agents_table.php
+++ database/migrations/2021_03_17_105733_add_referer_amount_to_agents_table.php
@@ -14,7 +14,7 @@ class AddRefererAmountToAgentsTable extends Migration
     public function up()
     {
         Schema::table('agents', function (Blueprint $table) {
-            //
+            $table->text('refererAmount')->nullable();
         });
     }
 
@@ -26,7 +26,7 @@ class AddRefererAmountToAgentsTable extends Migration
     public function down()
     {
         Schema::table('agents', function (Blueprint $table) {
-            //
+            $table->dropColumn('refererAmount');
         });
     }
 }
