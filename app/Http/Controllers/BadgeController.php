74f873317e293909c50e3ba0373133f568730e46
fmshot2 <femishotolaa@gmail.com>
2021-03-23 10:51:03 +0100

    changed badge controller gtpay to live credentials

--- app/Http/Controllers/BadgeController.php
+++ app/Http/Controllers/BadgeController.php
@@ -72,7 +72,7 @@ class BadgeController extends Controller
 
 
 	public function gtPAyForRegistration(Request $request) {
-		$gtpay_mert_id        = 14264;
+		$gtpay_mert_id        = 14435;
     	$gtpay_tranx_id      = $this->gen_transaction_id();
     	$gtpay_tranx_amt      = $request->amount * 100;
     	$gtpay_tranx_curr     = 566;
@@ -83,7 +83,7 @@ class BadgeController extends Controller
     	$gtpay_echo_data      = "{$request->user()->id},{$request->badge_type}";
     	$gtpay_no_show_gtbank = 'yes';
     	$gtpay_gway_name      = 'etranzact';
-    	$hashkey = '3EBF9CF6D082C89F88490B01D072B0F4E1EE52E86EC731D9B49538F33B551D486AB70673FE1B876B94EF76EC5E0AA1D3D14BA933424037FB1219662AFAB8FF51';
+    	$hashkey = '6470B923CDDE833E02B4CA0329432E8BF29B62B29B6B722397924F40731D44D8324AFE100EE2A4B6BD1299606A7C46D6BF0FF95220C3065F02DC052E7BFE5283';
 
     	 $gtpay_hash = $gtpay_mert_id.$gtpay_tranx_id.$gtpay_tranx_amt.$gtpay_tranx_curr.$gtpay_cust_id.$gtpay_tranx_noti_url.$hashkey;
 
