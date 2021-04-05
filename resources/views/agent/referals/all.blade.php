a85b65150bf5626046005a1351ed7e36cb1bedbe
fmshot2 <femishotolaa@gmail.com>
2021-04-05 17:32:29 +0100

    many

--- resources/views/agent/referals/all.blade.php
+++ resources/views/agent/referals/all.blade.php
@@ -22,7 +22,7 @@
             <div class="box" >
                <div class="box-header">
                   <h3 class="box-title"> Referals </h3>
-                  <h5 class="text-success downline_text" style="display: none;"> These are the benefits you've gained from this particular referer </h5>
+                  <h5 class="text-success downline_text" style="display: none;"> These are the bonuses you've gained from this particular referer </h5>
               </div>
               @if(isset($myreferrals))
 
@@ -52,7 +52,7 @@
                                         <tr role="row" class="odd">
                                            <td><a href="javascript:void(0)"> {{ $key + 1 }} </a></td>
                                            <!-- <td> {{ $myreferral->user->name }} </td> -->
-                                           <td> {{ $myreferral->user->refererLink }} </td>
+                                           <td> {{ $myreferral->user->name }} </td>
                                            <td> {{ $myreferral->user->refererAmount }} </td>
                                            <td> {{ $myreferral->created_at->diffForHumans() }} </td>
                                            <!-- <td> {{ $myreferral->status == 1 ? 'read' : 'unread' }} </td> -->
@@ -85,7 +85,7 @@
                                  <!-- <th> S/N </th> -->
 <!--                        <th> Referee Name </th>
 -->                       
-<th> Referer Link </th>
+<th> Referer Name </th>
 <!-- <th> Agent Code </th> -->
 <th> Level 1 Bonus </th>
 <th> Level 2 Bonus </th>
@@ -106,7 +106,7 @@
 
                                         <tr role="row" class="odd">
                                            <!-- <td><a href=""> </a></td> -->
-                                           <td id="ref_link">  </td>
+                                           <td id="name">  </td>
                                            <td id="level1">  </td>
                                            <td id="level2">  </td>
                                            <td id="level3">  </td>
@@ -127,9 +127,9 @@
                         </div>
                     </div>
                     <!-- /.box-body -->
-                   <!--  <div>
+                    <div>
                         <a class="btn btn-success downline_text" style="display: none;" onclick="goBack()">Go Back</a>
-                    </div> -->
+                    </div>
 
 
                 </div>
@@ -161,7 +161,7 @@
                 $('#referer_text').css("display", "none");
 
                                 // $("#sub_end2").innerHTML = data.new_date;
-                                $("#ref_link").html(data.success.refererLink);
+                                $("#name").html(data.success.name);
                                 if (data.success.level1) {
                                     var level1_value = 200;
                                    $("#level1").html(200);
@@ -211,75 +211,5 @@
           window.history.back();
     }
 </script>
-
-
-
-<script>
-
-    base_Url = "{{ url('/') }}"
-
-    var _token = $("input[name='_token']").val();
-
-                // var email1 = $("#email-address3").val();
-                var amount = $("#sub_cost").val();
-                var sub_type = $("#sub_type").val();
-
-                function payWithPaystack1() {
-                    $('#badgeRequestModal').modal('hide');
-                    console.log(base_Url);
-
-                    var handler = PaystackPop.setup({
-                        key: 'pk_test_b951412d1d07c535c90afd8a9636227f54ce1c43',
-                        email: document.getElementById("email-address3").value,
-                        amount: $("#sub_cost").val(),
-                        ref: '' + Math.floor((Math.random() * 1000000000) + 1),
-                        metadata: {
-                            custom_fields: [{
-                                display_name: "Mobile Number",
-                                variable_name: "mobile_number",
-                                value: "+2348012345678"
-                            }]
-                        },
-                        callback: function(response) {
-
-                            var email = document.getElementById("email-address3").value;
-                            var amount = $("#sub_cost").val();
-                            var ref_no1 =  response.reference;
-                            var sub_type = $("#sub_type").val();
-                            console.log(email, amount, ref_no1, sub_type)
-                            
-                            $.ajax({
-                              method: "POST",
-                              url: base_Url + '/provider/service/create_sub',
-                              dataType: "json",
-                              data: {
-                                _token: _token,
-                                email: email,
-                                amount: amount,
-                                ref_no: ref_no1,
-                                sub_type: sub_type
-                            },
-                            success: function (data) {
-                                console.log(data);
-                                toastr.success('You have purchased a new subscription!')
-                                // $("#sub_end2").innerHTML = data.new_date;
-                                $("#sub_end2").html(data.new_date);
-                                 // $('#2').css("display", "block");
-                             },
-                             error: function(error) {
-                              console.log(error)
-                          }
-                      })
-                        },
-                        onClose: function() {
-                            alert('window closed');
-                        }
-                    });
-                    handler.openIframe();
-                }
-
-
-
-            </script>
 
             @endsection
