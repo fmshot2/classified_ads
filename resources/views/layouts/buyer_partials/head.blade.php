1b9fb1e3dc59ed91e64a2d55e7fbe0d6ca912079
Ibidapo Adeolu <adeoluibidapo@gmail.com>
2021-04-11 12:11:34 +0100

    Livechat

--- resources/views/layouts/buyer_partials/head.blade.php
+++ resources/views/layouts/buyer_partials/head.blade.php
@@ -69,5 +69,20 @@
 
 <script src="{{ asset('js/jquery-2.2.0.min.js') }}"></script>
 <script src="{{ asset('toastr/toastr.min.js') }}"></script>
+
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+        var s1 = document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async = true;
+        s1.src = 'https://embed.tawk.to/5ff49fb2c31c9117cb6bba8f/1er9ovkca';
+        s1.charset = 'UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+    })();
+</script>
+<!--End of Tawk.to Script-->
+
 </head>
 
