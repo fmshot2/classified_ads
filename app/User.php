209b17a722a0a529017619c85ae8cd3ca835caf7
unknown <fmshot2>
2021-04-23 12:53:55 +0100

    removed has many payments function on User php

--- app/User.php
+++ app/User.php
@@ -62,9 +62,9 @@ class User extends Authenticatable implements MustVerifyEmail, JWTSubject
         return $this->hasMany('\App\Service'); //Product Model Name
     }
 
-    public function payments(){
-        return $this->hasMany('\App\Payment'); //Product Model Name
-    }
+    // public function payments(){
+    //     return $this->hasMany('\App\Payment'); //Product Model Name
+    // }
 
     public function seeking_works(){
         return $this->hasMany('\App\SeekingWork'); //Product Model Name
