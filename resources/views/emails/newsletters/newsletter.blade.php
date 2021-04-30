a6c6d2d417be60d35dea31946a8a85cb94cd6dd5
Ibidapo Adeolu <bhekor@users.noreply.github.com>
2021-04-30 15:09:30 +0100

    lots

--- resources/views/emails/newsletters/newsletter.blade.php
+++ resources/views/emails/newsletters/newsletter.blade.php
@@ -12,7 +12,7 @@
 
 <h3 style="box-sizing: border-box; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'; position: relative; color: #3d4852; font-size: 19px; font-weight: bold; margin-top: 0; text-align: center;">Hello, {{ $username }}!</h3>
 
-<h5 style="box-sizing: border-box; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'; position: relative; color: #3d4852; font-size: 15px; font-weight: bold; margin-top: -10px; text-align: center;">We have found some awesome services for you!</h5>
+<h5 style="box-sizing: border-box; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'; position: relative; color: #3d4852; font-size: 15px; font-weight: bold; margin-top: -10px; text-align: center;">We have found some awesome products and services for you!</h5>
 
 {{-- <h4 style="font-size: 18px; margin-bottom: -30px;padding-left:20px;color:#CA8309;text-transform:uppercase">{{ $category->name }}</h4> --}}
 <div class="table" style="box-sizing: border-box; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'; position: relative;"></div>
@@ -40,8 +40,7 @@ Thanks,<br>
 </div>
 
 <div class="table" style="box-sizing: border-box; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'; padding-bottom: 15px; text-align: center; font-size: 13px">
-    Please do not reply to this email. Click here to <a style="text-decoration: none; color: #ca8309" href="mailto:{{ $general_info->hot_line ? $general_info->support_email : '' }}">unsubscribe.</a>
-Please do not reply to this email.
+    Please do not reply to this email.
 
 <p style="font-size:13px; line-height:1.5; text-align:justify"><small><strong style="font-size:11px;">DISCLAIMER:</strong> This email and any information transmitted with it are confidential and intended solely for the use of the individual or entity to whom they are addressed. If you have received this email in error please notify the system manager. This message contains confidential information and is intended only for the individual named. If you are not the named addressee you should not disseminate, distribute or copy this e-mail. Please notify the sender immediately by e-mail if you have received this e-mail by mistake and delete this e-mail from your system. If you are not the intended recipient you are notified that disclosing, copying, distributing or taking any action in reliance on the contents of this information is strictly prohibited. If you are the intended please note that you are not to share or disclose the content of this mail.</small></p>
 </div>
