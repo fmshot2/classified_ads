d329492c97a45a0e57d32b36d592b2d7c308fa5b
fmshot2 <femishotolaa@gmail.com>
2021-06-01 12:14:43 +0100

    added like api

--- routes/api.php
+++ routes/api.php
@@ -57,6 +57,8 @@ Route::group([
         Route::post('service/create', [ServiceController::class, 'createService']);
         Route::delete('service/delete/{id}', [ServiceController::class, 'deleteService']);
         Route::put('service/update', [ServiceController::class, 'updateService']);
+        Route::get('service/saveLike', [ServiceController::class, 'saveLike2']);
+
 
         // SEEKING WORK
         Route::post('seeking-work/create', [ServiceController::class, 'seekingWorkCreate']);
